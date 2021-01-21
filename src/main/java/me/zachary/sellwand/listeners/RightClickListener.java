c0a89853399d107504481abd3a41baf063d0546b
Zach_FR <zac232003@icloud.com>
2021-01-21 15:25:37 -0500

    Added support of Shulker, Trapped chest and barrel

--- src/main/java/me/zachary/sellwand/listeners/RightClickListener.java
+++ src/main/java/me/zachary/sellwand/listeners/RightClickListener.java
@@ -11,14 +11,20 @@ import me.zachary.zachcore.utils.items.ItemBuilder;
 import me.zachary.zachcore.utils.xseries.XMaterial;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.Barrel;
 import org.bukkit.block.Chest;
+import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class RightClickListener implements Listener {
     private Sellwand plugin;
@@ -30,10 +36,43 @@ public class RightClickListener implements Listener {
 
     @EventHandler
     public void onRightClick(PlayerInteractEvent event){
-        if(event.getAction() != Action.RIGHT_CLICK_BLOCK || event.getHand() == EquipmentSlot.OFF_HAND || event.getClickedBlock().getBlockData().getMaterial() != Material.CHEST)
+        if(event.getAction() != Action.RIGHT_CLICK_BLOCK || event.getHand() == EquipmentSlot.OFF_HAND)
             return;
         Player player = event.getPlayer();
-        Chest chestClicked = (Chest) event.getClickedBlock().getState();
+        Inventory contents = null;
+        List<Material> shulkerBoxMaterial = Arrays.asList(
+                Material.SHULKER_BOX,
+                Material.WHITE_SHULKER_BOX,
+                Material.ORANGE_SHULKER_BOX,
+                Material.MAGENTA_SHULKER_BOX,
+                Material.LIGHT_BLUE_SHULKER_BOX,
+                Material.YELLOW_SHULKER_BOX,
+                Material.LIME_SHULKER_BOX,
+                Material.PINK_SHULKER_BOX,
+                Material.GRAY_SHULKER_BOX,
+                Material.LIGHT_BLUE_SHULKER_BOX,
+                Material.CYAN_SHULKER_BOX,
+                Material.PURPLE_SHULKER_BOX,
+                Material.BLUE_SHULKER_BOX,
+                Material.BROWN_SHULKER_BOX,
+                Material.GREEN_SHULKER_BOX,
+                Material.CYAN_SHULKER_BOX,
+                Material.RED_SHULKER_BOX,
+                Material.BLACK_SHULKER_BOX
+        );
+        if(event.getClickedBlock().getBlockData().getMaterial() == Material.CHEST
+                || event.getClickedBlock().getBlockData().getMaterial() == Material.TRAPPED_CHEST){
+            Chest chests = (Chest) event.getClickedBlock().getState();
+            contents = chests.getInventory();
+        }else if(shulkerBoxMaterial.contains(event.getClickedBlock().getBlockData().getMaterial())){
+            ShulkerBox shulkerBox = (ShulkerBox) event.getClickedBlock().getState();
+            contents = shulkerBox.getInventory();
+        }else if(event.getClickedBlock().getBlockData().getMaterial() == Material.BARREL){
+            Barrel barrel = (Barrel) event.getClickedBlock().getState();
+            contents = barrel.getInventory();
+        }
+        if(contents == null || contents.getContents() == null)
+            return;
         NBTItem item = null;
         double amount = 0D;
         int itemAmount = 0;
@@ -54,13 +93,13 @@ public class RightClickListener implements Listener {
                 return;
             }
             double multiplier = item.getDouble("Multiplier");
-            for (int i = 0; i < chestClicked.getInventory().getContents().length; i++){
-                ItemStack chestItem = chestClicked.getInventory().getItem(i);
+            for (int i = 0; i < contents.getContents().length; i++){
+                ItemStack chestItem = contents.getItem(i);
                 Double price = 0D;
                 if(chestItem != null)
                     price = plugin.getItemPrice(player, chestItem);
                 if(price != -1.0 && chestItem != null){
-                    chestClicked.getInventory().setItem(i, new ItemBuilder(XMaterial.AIR.parseMaterial()).build());
+                    contents.setItem(i, new ItemBuilder(XMaterial.AIR.parseMaterial()).build());
                     itemAmount += chestItem.getAmount();
                     amount += price;
                 }
